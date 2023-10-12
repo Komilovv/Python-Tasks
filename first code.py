89440f1169a575e5f16a7564b03f47272d8b40fd
Komilovv <otabekkomilov515@gmail.com>
2023-10-13 00:25:27 +0900

    first
    I will be master of AI and Cyber  security till graduating my bachalor degree.

---
+++ first code.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 23:47:13 2023
+
+@author: ADMIN
+"""
+
+print('"Tico",\'Nexia\',"Damas" ko`rganlar qilar havas')
+# 5 ning 4 -darajasini toping
+print(5**4)
+#22 ni 4 ga bo`lganda qancha qoldiq qoladi?
+print(22%4)
+# Tomonlari 125 ga teng bo`lgan kvadratning yuzi va perimetirini toping.
+print("yuzi=",125**2,"perimetri=",125*4,)
+#Diametri 12 ga teng bo`lgan doiraning yuzini toping. (p=3.14 deb olinsin)
+print("S=",3.14*6**2)
+# Katetlari 6 va 7 bo`lgan to`g`ri burchakli uchburchakning gipotenuzasini toping.
+print((6**2+7**2)**(1/2))
